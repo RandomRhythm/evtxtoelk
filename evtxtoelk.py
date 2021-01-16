979c16962a397df0ef4a286696f8c622f405bbf5
Ryan B <randomrhythm@rhythmengineering.com>
2021-01-16 15:06:55 -0700

    Support pointing at a folder to import all contained evtx files and moving files to a directory once imported.
    Signed-off-by: Ryan B <randomrhythm@rhythmengineering.com>

--- evtxtoelk.py
+++ evtxtoelk.py
@@ -11,7 +11,7 @@ from Evtx.Views import evtx_file_xml_view
 from elasticsearch import Elasticsearch, helpers
 import xmltodict
 import sys
-
+import os
 
 class EvtxToElk:
     @staticmethod
@@ -113,7 +113,7 @@ class EvtxToElk:
                                 bulk_queue = []
                             else:
                                 print('Failed to bulk data to Elasticsearch')
-                                sys.exit(1)
+                                #sys.exit(1)
 
                     except:
                         print("***********")
@@ -129,7 +129,7 @@ class EvtxToElk:
                         bulk_queue = []
                     else:
                         print('Failed to bulk data to Elasticsearch')
-                        sys.exit(1)
+                        #sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -141,6 +141,20 @@ if __name__ == "__main__":
     parser.add_argument('-i', default="hostlogs", help="ELK index to load data into")
     parser.add_argument('-s', default=500, help="Size of queue")
     parser.add_argument('-meta', default={}, type=json.loads, help="Metadata to add to records")
+    parser.add_argument('-movefolder', default="", help="Move completed evtx files to this folder")
+
     # Parse arguments and call evtx to elk class
     args = parser.parse_args()
-    EvtxToElk.evtx_to_elk(args.evtxfile, args.elk_ip, elk_index=args.i, bulk_queue_len_threshold=int(args.s), metadata=args.meta)
+    if os.path.isdir(args.evtxfile):  
+        for (dirpath, dirnames, filenames) in os.walk(args.evtxfile):
+          for file in filenames:
+              scanPath = os.path.join(dirpath, file)
+              print(scanPath + "\n")
+              EvtxToElk.evtx_to_elk(scanPath, args.elk_ip, elk_index=args.i, bulk_queue_len_threshold=int(args.s), metadata=args.meta)
+              if args.movefolder != "":
+                  os.replace(scanPath, os.path.join(args.movefolder, file))
+ 
+    elif os.path.isfile(args.evtxfile):  
+        EvtxToElk.evtx_to_elk(args.evtxfile, args.elk_ip, elk_index=args.i, bulk_queue_len_threshold=int(args.s), metadata=args.meta)
+        if args.movefolder != "":
+            os.replace(scanPath, os.path.join(args.movefolder, file))
